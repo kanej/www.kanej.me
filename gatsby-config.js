e85be40e154d4ff6dd58f0b2bf52fa69e796a868
John Kane <john@kanej.me>
2019-07-10 21:55:08 +0100

    Add build as a predeploy step

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,6 @@ module.exports = {
     'gatsby-plugin-ipfs'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    //'gatsby-plugin-offline',
   ]
 }
