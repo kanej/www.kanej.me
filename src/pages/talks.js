e5dca3e69b3aae0775b67a9e8b3b82404265cfab
John Kane <john@kanej.me>
2019-10-11 15:31:23 +0100

    fix: fix broken links in talks page

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,44 +1,57 @@
 import React from 'react'
-import { graphql, Link  } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 export default ({ data }) => {
   return (
     <Layout>
-        <header>
-            <h2>Talks</h2>
-        </header>
-        <p>In case you wanted slides from a talk I have given, here they are, you poor misguided soul...</p>
-        {data.allMarkdownRemark.edges.map(({ node }) => {
-            const slidesLink = <Link key={`${node.frontmatter.title}-slides`} to="{node.frontmatter.slides}">slides</Link>
-            const audioLink = node.frontmatter.audio
-                ? <Link key={`${node.frontmatter.title}-audio`} to="{{node.frontmatter.audio}}">audio</Link>
-                : null
-            const talkLinks = node.frontmatter.audio
-                ? [slidesLink, " | ", audioLink]
-                : slidesLink
+      <header>
+        <h2>Talks</h2>
+      </header>
+      <p>
+        In case you wanted slides from a talk I have given, here they are, you
+        poor misguided soul...
+      </p>
+      {data.allMarkdownRemark.edges.map(({ node }) => {
+        const slidesLink = (
+          <a
+            key={`${node.frontmatter.title}-slides`}
+            href={`${node.frontmatter.slides}`}
+          >
+            slides
+          </a>
+        )
+        const audioLink = node.frontmatter.audio ? (
+          <a
+            key={`${node.frontmatter.title}-audio`}
+            href={`${node.frontmatter.audio}`}
+          >
+            audio
+          </a>
+        ) : null
+        const talkLinks = node.frontmatter.audio
+          ? [slidesLink, ' | ', audioLink]
+          : slidesLink
 
-            return (
-                <div key={node.frontmatter.title} className="talk-box">
-                    <div className="talk-header">
-                        <h3 style={{float: 'left'}}>{node.frontmatter.title}</h3>
-                        
-                        <div style={{float: 'right'}}>
-                            <p>{node.frontmatter.date}</p>
-                        </div>
-                    </div>
-                    <div className="talk-venue">
-                        <h5>{node.frontmatter.venue}</h5>
-                    </div>  
-                    <div className="talk-content">
-                        <div dangerouslySetInnerHTML={{ __html: node.html }} />
-                    </div>
-                    <div className="talk-links">
-                        {talkLinks}
-                    </div>
-                </div>
-            )
-        })}
+        return (
+          <div key={node.frontmatter.title} className="talk-box">
+            <div className="talk-header">
+              <h3 style={{ float: 'left' }}>{node.frontmatter.title}</h3>
+
+              <div style={{ float: 'right' }}>
+                <p>{node.frontmatter.date}</p>
+              </div>
+            </div>
+            <div className="talk-venue">
+              <h5>{node.frontmatter.venue}</h5>
+            </div>
+            <div className="talk-content">
+              <div dangerouslySetInnerHTML={{ __html: node.html }} />
+            </div>
+            <div className="talk-links">{talkLinks}</div>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
@@ -46,8 +59,9 @@ export default ({ data }) => {
 export const query = graphql`
   query {
     allMarkdownRemark(
-      filter: {fileAbsolutePath: {regex: "/talks/"}},
-      sort: {fields: [frontmatter___date], order: DESC}) {
+      filter: { fileAbsolutePath: { regex: "/talks/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       edges {
         node {
           frontmatter {
